ae3afed81f6ff84256b4f19de0c27461fefbc5ed
João Lopes <jp.lopes.melo01@gmail.com>
2025-09-12 11:12:43 -0300

    Consegui fazer um subscriber que detecta a cor e mostra no terminal

--- ROS/camera_pub.py
+++ ROS/camera_pub.py
@@ -18,10 +18,13 @@ def camera_pub(cap):
         if not ret:
             rospy.loginfo("Erro na abertura da câmera.")
             break
-        msg = bridge.cv2_to_imgmsg(frame, "bgr8")
-        pub.publish(msg)
-        if cv2.waitKey(1) == ord('q'):
-            break
+        else: 
+            try:
+                msg = bridge.cv2_to_imgmsg(frame, "bgr8")           #Conversão da imagem cv2 para msg
+            except CvBridgeError:
+                rospy.loginfo("Erro na conversão de imagem.")
+            else:
+                pub.publish(msg)
     rate.sleep()
 
 if __name__ == '__main__':
