6828397c730a3c6741ccf0ebe98671f6f3484189
João Lopes <jp.lopes.melo01@gmail.com>
2025-11-03 20:31:56 -0300

    Agora o detector de comandos e o voz_pub sao um só

--- ROS OFICIAL SIRIGUEIJO/controle_por_voz.py
+++ ROS OFICIAL SIRIGUEIJO/controle_por_voz.py
@@ -1,23 +1,65 @@
 #!/usr/bin/env python3
 
+import speech_recognition as sr
 import rospy
 from std_msgs.msg import String
+from unidecode import unidecode
 
-lista_comandos = ['ativar camera', 'desativar camera', 
-            'ativar voz', 'desativar voz', 
-            'ativar movimentos', 'desativar movimentos']
+comandos = ['ativar camera', 'ativar cor', 'ativar gestos', 'desativar tudo']
+palavras_chave = ['dinheiro', 'plankton', 'plancton', 'lula', 'molusco', 'platelminto']
 
-def callback(data, pub):
-    frase = data.data
-    if frase in lista_comandos:
-        rospy.loginfo("Comando detectado: " + frase.upper())
-        pub.publish(frase.upper()) #publica com letra maiscula
+class VozNode:
+    def __init__(self):
+        self.r = sr.Recognizer()
 
-def detectar_comando():
-    rospy.init_node('controle_por_voz')
-    pub = rospy.Publisher('comando_detectado', String, queue_size=10)
-    rospy.Subscriber('frases', String, callback, callback_args=pub)
-    rospy.spin()
 
+    def detectarComandosDeVoz(self):
+        pub = rospy.Publisher('comandos', String, queue_size=10)
+        rospy.init_node('controle_por_voz')
+        rate = rospy.Rate(20)
+
+        with sr.Microphone() as source:
+            rospy.loginfo("Ajustando para o ruído ambiente. Por favor, aguarde...")
+            self.r.adjust_for_ambient_noise(source, duration=1)
+            rospy.loginfo("Pronto! Fale agora...")
+            
+            try:
+                while not rospy.is_shutdown():
+                    try:
+                        audio = self.r.listen(source, timeout=2, phrase_time_limit=3)
+                        texto_transcrito = self.r.recognize_google(audio, language="pt-BR")
+                    except sr.WaitTimeoutError:
+                        pass
+                    except sr.UnknownValueError:
+                        pass
+                    except sr.RequestError as e:
+                        rospy.loginfo(f"Erro na requisição; {e}")
+                    except Exception as e:
+                        rospy.loginfo(f"Ocorreu um erro: {e}")
+                    else:
+                        if texto_transcrito:
+                            texto_processado = unidecode(texto_transcrito) # remove acentos da string
+                            texto_processado = texto_processado.lower() # transforma para letra minuscula
+
+                            if texto_processado in comandos:
+                                rospy.loginfo("Comando detectado: " + texto_processado.upper())
+                                pub.publish(texto_processado.upper()) #publica com letra maiscula
+                            
+                            frase = texto_processado.split()
+                            for palavra in frase:
+                                if palavra in palavras_chave:
+                                    rospy.loginfo("Palavra chave detectada: " + palavra)
+                                    pub.publish(palavra)
+                                    break
+                    rate.sleep()
+            except KeyboardInterrupt:
+                pass
+            finally:
+                rospy.loginfo("Encerrando nó...")
+        
 if __name__ == '__main__':
-    detectar_comando()
+    try:
+        controleVoz = VozNode()
+        controleVoz.detectarComandosDeVoz()
+    except rospy.ROSInterruptException:
+        pass
